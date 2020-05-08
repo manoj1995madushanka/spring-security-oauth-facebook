141660e0bac0539b2c3178d9d51d8e1acc71abe9
manoj1995madushanka <madushankamanoj414@gmail.com>
2020-05-08 23:11:18 +0530

    init

---
+++ src/main/java/com/example/oauthspring/OauthSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oauthspring/OauthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
