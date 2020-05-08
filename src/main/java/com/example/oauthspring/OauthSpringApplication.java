ef246c6b85ddb91e4dc3a1a5d5740d3d56ec8798
manoj1995madushanka <madushankamanoj414@gmail.com>
2020-05-08 23:37:40 +0530

    add annotation

--- src/main/java/com/example/oauthspring/OauthSpringApplication.java
+++ src/main/java/com/example/oauthspring/OauthSpringApplication.java
@@ -2,8 +2,11 @@ package com.example.oauthspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class OauthSpringApplication {
 
     public static void main(String[] args) {
